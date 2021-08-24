a6ddf817ab02e699eb4c38d22da11460f3331691
d4nielj <d4niel.djm@gmail.com>
2021-08-23 19:17:06 -0500

    Add books to initial state

--- src/redux/books/booksReducer.js
+++ src/redux/books/booksReducer.js
@@ -1,6 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = [];
+const initialState = [
+  {
+    id: 1,
+    title: 'Lord of the Rings',
+    author: 'Tolkien',
+    genre: 'Fantasy',
+  },
+  {
+    id: 2,
+    title: 'Game of Thrones',
+    author: 'Martin',
+    genre: 'Fantasy',
+  },
+  {
+    id: 3,
+    title: '1984',
+    author: 'Asimov',
+    genre: 'Science Fiction',
+  },
+];
 
 export const booksSlice = createSlice({
   name: 'books',
