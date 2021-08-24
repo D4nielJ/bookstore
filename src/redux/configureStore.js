198e0c1ce5bb0456547f6bf4633c6bec659b32ce
d4nielj <d4niel.djm@gmail.com>
2021-08-23 22:12:41 -0500

    Change enhancer

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,15 +1,14 @@
-import { createStore, combineReducers } from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
 import booksReducer from './books/booksReducer';
 
 const reducer = combineReducers({
   books: booksReducer,
 });
 
-/* eslint-disable no-underscore-dangle */
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logger)
 );
-/* eslint-enable */
 
 export default store;
