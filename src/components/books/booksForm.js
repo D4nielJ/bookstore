dc03afe454fb5beae18e316541bd67fffb73a4f7
d4nielj <d4niel.djm@gmail.com>
2021-08-23 19:44:19 -0500

    Add input for author in form

--- src/components/books/booksForm.js
+++ src/components/books/booksForm.js
@@ -6,26 +6,29 @@ import { addBook } from '../../redux/books/booksReducer';
 
 const BooksForm = () => {
   const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
   const [genre, setGenre] = useState('Action');
 
   const dispatch = useDispatch();
 
   const onTitleChanged = (e) => setTitle(e.target.value);
+  const onAuthorChange = (e) => setAuthor(e.target.value);
   const onGenreChanged = (e) => setGenre(e.target.value);
 
   const onAddBookClicked = (e) => {
     e.preventDefault();
-    if (title && genre) {
+    if (title && genre && author) {
       dispatch(
         addBook({
           id: nanoid(),
           title,
           genre,
-          author: 'Jhon Doe',
+          author,
         }),
       );
 
       setTitle('');
+      setAuthor('');
       setGenre('Action');
     }
   };
@@ -39,15 +42,23 @@ const BooksForm = () => {
         id="title"
         value={title}
         onChange={onTitleChanged}
+        placeholder="Title"
+        required
+      />
+      <input
+        type="text"
+        name="author"
+        id="author"
+        value={author}
+        onChange={onAuthorChange}
+        placeholder="Author"
         required
       />
       <select name="genre" id="genre" value={genre} onChange={onGenreChanged}>
         <option value="Action">Action</option>
         <option value="Science Fiction">Science Fiction</option>
       </select>
-      <button type="submit">
-        ADD BOOK
-      </button>
+      <button type="submit">ADD BOOK</button>
     </form>
   );
 };
